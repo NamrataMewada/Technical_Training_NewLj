0a03a82a6fbbde21b4c3cf7212e0dd50c33196cd
NamrataMewada <132078859+NamrataMewada@users.noreply.github.com>
2024-07-21 22:06:58 +0530

    Add files via upload

---
+++ Training1/Division.java
@@ -0,0 +1,13 @@
+package Training1;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int num1 = 2;
+		int num2 = 3;
+		float ans = (float)num1/num2;
+		System.out.println("Anwer is :"+ans);
+
+	}
+
+}
---
+++ Training1/Loops.java
@@ -0,0 +1,25 @@
+package Training1;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		System.out.println("Even Numbers from 1 to 30 using a while loop:");
+		int i=1;
+		while(i<=30){
+			if(i%2==0){
+				System.out.println(i);
+			}i++;
+		}
+		
+		System.out.println("Odd Numbers from 1 to 30 using a do-while loop:");
+		int j=1;
+		do {
+			if(j%2!=0) {
+				System.out.println(j);	
+			}
+			j++;
+		}while(j<=30);
+
+	}
+
+}
